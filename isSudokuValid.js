66f8b294c2013095132024b2096c3d21a619e7a6
musayuksel <musayuxel@gmail.com>
2022-08-31 23:15:58 +0100

    Update subgrid adding conditons

--- isSudokuValid.js
+++ isSudokuValid.js
@@ -11,33 +11,12 @@ function isSudokuValid(board) {
   board.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
       columns[columnIndex][rowIndex] = column;
-      if (rowIndex < 3) {
-        //sub-grid 0-1-2
-        if (columnIndex < 3) {
-          subGrids[0].push(column);
-        } else if (columnIndex < 6) {
-          subGrids[1].push(column);
-        } else if (columnIndex < 9) {
-          subGrids[2].push(column);
-        }
-      } else if (rowIndex < 6) {
-        //sub-grid 3-4-5
-        if (columnIndex < 3) {
-          subGrids[3].push(column);
-        } else if (columnIndex < 6) {
-          subGrids[4].push(column);
-        } else if (columnIndex < 9) {
-          subGrids[5].push(column);
-        }
-      } else {
-        //sub-grid 6-7-8
-        if (columnIndex < 3) {
-          subGrids[6].push(column);
-        } else if (columnIndex < 6) {
-          subGrids[7].push(column);
-        } else if (columnIndex < 9) {
-          subGrids[8].push(column);
-        }
+      if (columnIndex < 3) {
+        subGrids[Math.floor(rowIndex / 3) * 3 + 0].push(column);
+      } else if (columnIndex < 6) {
+        subGrids[Math.floor(rowIndex / 3) * 3 + 1].push(column);
+      } else if (columnIndex < 9) {
+        subGrids[Math.floor(rowIndex / 3) * 3 + 2].push(column);
       }
     });
   });
@@ -54,7 +33,6 @@ const board = [
   [6, 2, 5, 9, 4, 8, 1, 3, 7],
   [8, 7, 3, 5, 1, 2, 9, 6, 4],
 ];
-console.log(isSudokuValid(board)); // true
 
 const board2 = [
   [8, 2, 9, 3, 6, 5, 1, 4, 7],
@@ -67,4 +45,4 @@ const board2 = [
   [2, 6, 7, 8, 3, 4, 9, 5, 1],
   [9, 3, 5, 2, 7, 9, 8, 6, 4],
 ];
-console.log(isSudokuValid(board2)); // true
+console.log(isSudokuValid(board), isSudokuValid(board2)); // true - false
