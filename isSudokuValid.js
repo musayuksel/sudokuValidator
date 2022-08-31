f3c8ec5001ce61aa070484c837c5d3b7b4b4f521
musayuksel <musayuxel@gmail.com>
2022-08-31 22:40:38 +0100

    Add subGrids and validation

--- isSudokuValid.js
+++ isSudokuValid.js
@@ -2,7 +2,7 @@ function isSudokuValid(board) {
   const isValid = (rows) => {
     return rows.every((row) => {
       row.sort((first, second) => first - second);
-      console.log(row);
+      // console.log(row);
       return row.join("") === "123456789";
     });
   };
@@ -10,14 +10,43 @@ function isSudokuValid(board) {
   // find columns and sub-grids grid
   // check rows, columns, and sub-grids should be valid
   const rows = [...board];
-  console.log(rows);
   const columns = [[], [], [], [], [], [], [], [], []]; //[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]....]
+  const subGrids = [[], [], [], [], [], [], [], [], []]; //[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]....]
   board.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
       columns[columnIndex][rowIndex] = column;
+      if (rowIndex < 3) {
+        //sub-grid 0-1-2
+        if (columnIndex < 3) {
+          subGrids[0].push(column);
+        } else if (columnIndex < 6) {
+          subGrids[1].push(column);
+        } else if (columnIndex < 9) {
+          subGrids[2].push(column);
+        }
+      } else if (rowIndex < 6) {
+        //sub-grid 3-4-5
+        if (columnIndex < 3) {
+          subGrids[3].push(column);
+        } else if (columnIndex < 6) {
+          subGrids[4].push(column);
+        } else if (columnIndex < 9) {
+          subGrids[5].push(column);
+        }
+      } else {
+        //sub-grid 6-7-8
+        if (columnIndex < 3) {
+          subGrids[6].push(column);
+        } else if (columnIndex < 6) {
+          subGrids[7].push(column);
+        } else if (columnIndex < 9) {
+          subGrids[8].push(column);
+        }
+      }
     });
   });
-  return isValid(rows) && isValid(columns);
+  console.log({ subGrids });
+  return isValid(rows) && isValid(columns) && isValid(subGrids);
 }
 const board = [
   [1, 5, 2, 4, 8, 9, 3, 7, 6],
