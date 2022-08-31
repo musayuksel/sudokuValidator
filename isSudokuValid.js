8078118800b6a8a68f97132133519ac24b6c539d
musayuksel <musayuxel@gmail.com>
2022-08-31 22:46:12 +0100

    Update isValid function and delete comments

--- isSudokuValid.js
+++ isSudokuValid.js
@@ -1,17 +1,13 @@
 function isSudokuValid(board) {
-  const isValid = (rows) => {
-    return rows.every((row) => {
-      row.sort((first, second) => first - second);
-      // console.log(row);
-      return row.join("") === "123456789";
-    });
-  };
-  // return isValid(board);
-  // find columns and sub-grids grid
-  // check rows, columns, and sub-grids should be valid
+  const isValid = (rows) =>
+    rows.every(
+      (row) =>
+        row.sort((first, second) => first - second).join("") === "123456789"
+    );
+
   const rows = [...board];
   const columns = [[], [], [], [], [], [], [], [], []]; //[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]....]
-  const subGrids = [[], [], [], [], [], [], [], [], []]; //[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]....]
+  const subGrids = [[], [], [], [], [], [], [], [], []];
   board.forEach((row, rowIndex) => {
     row.forEach((column, columnIndex) => {
       columns[columnIndex][rowIndex] = column;
@@ -45,7 +41,6 @@ function isSudokuValid(board) {
       }
     });
   });
-  console.log({ subGrids });
   return isValid(rows) && isValid(columns) && isValid(subGrids);
 }
 const board = [
@@ -60,3 +55,16 @@ const board = [
   [8, 7, 3, 5, 1, 2, 9, 6, 4],
 ];
 console.log(isSudokuValid(board)); // true
+
+const board2 = [
+  [8, 2, 9, 3, 6, 5, 1, 4, 7],
+  [6, 4, 3, 7, 1, 8, 5, 1, 9],
+  [7, 5, 1, 4, 9, 2, 6, 3, 8],
+  [3, 1, 8, 5, 2, 7, 4, 9, 6],
+  [5, 9, 6, 1, 4, 1, 7, 8, 2],
+  [4, 7, 2, 9, 8, 6, 3, 1, 5],
+  [9, 8, 4, 6, 5, 1, 2, 7, 3],
+  [2, 6, 7, 8, 3, 4, 9, 5, 1],
+  [9, 3, 5, 2, 7, 9, 8, 6, 4],
+];
+console.log(isSudokuValid(board2)); // true
