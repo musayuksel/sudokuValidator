34588a626ab5f3d253b10d3fb50f045271e31b76
musayuksel <musayuxel@gmail.com>
2022-08-31 21:12:02 +0100

    Add isValid function for rows

---
+++ isSudokuValid.js
@@ -0,0 +1,22 @@
+function isSudokuValid(board) {
+  const isValid = (rows) => {
+    return rows.every((row) => {
+      row.sort((first, second) => first - second);
+      console.log(row);
+      return row.join("") === "123456789";
+    });
+  };
+  return isValid(board);
+}
+const board = [
+  [1, 5, 2, 4, 8, 9, 3, 7, 6],
+  [7, 3, 9, 2, 5, 6, 8, 4, 1],
+  [4, 6, 8, 3, 7, 1, 2, 9, 5],
+  [3, 8, 7, 1, 2, 4, 6, 5, 9],
+  [5, 9, 1, 7, 6, 3, 4, 2, 8],
+  [2, 4, 6, 8, 9, 5, 7, 1, 3],
+  [9, 1, 4, 6, 3, 7, 5, 8, 2],
+  [6, 2, 5, 9, 4, 8, 1, 3, 7],
+  [8, 7, 3, 5, 1, 2, 9, 6, 4],
+];
+console.log(isSudokuValid(board)); // true
